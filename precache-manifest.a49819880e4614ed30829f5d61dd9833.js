2334141786dc2f94e14d0c4010bf3b06a46206a1
Vlad Rev <vergigo89@gmail.com>
2021-10-05 19:38:04 +0500

    Updates

---
+++ precache-manifest.a49819880e4614ed30829f5d61dd9833.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5868f3b2df75f60274dd3825c1f64006",
+    "url": "/Weather-Finder/index.html"
+  },
+  {
+    "revision": "bb44d9d54d6f7bef9ed0",
+    "url": "/Weather-Finder/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "ed1b4eb3de3df9c69ecb",
+    "url": "/Weather-Finder/static/css/main.9996696c.chunk.css"
+  },
+  {
+    "revision": "bb44d9d54d6f7bef9ed0",
+    "url": "/Weather-Finder/static/js/2.3de25347.chunk.js"
+  },
+  {
+    "revision": "ed1b4eb3de3df9c69ecb",
+    "url": "/Weather-Finder/static/js/main.1a6e7ea7.chunk.js"
+  },
+  {
+    "revision": "a551805f170dd99ffbce",
+    "url": "/Weather-Finder/static/js/runtime-main.15549c2a.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/Weather-Finder/static/media/weather.b6796c73.jpg"
+  }
+]);
